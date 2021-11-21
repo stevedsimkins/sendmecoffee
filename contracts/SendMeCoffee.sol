09b8be43d9e5a72a055fc5df244e8a0642d4c320
Steve <stevedsimkins@gmail.com>
2021-11-20 22:07:13 -0500

    added .call

--- contracts/SendMeCoffee.sol
+++ contracts/SendMeCoffee.sol
@@ -15,7 +15,7 @@ contract SendMeCoffee {
 
   Coffee[] coffees;
 
-  constructor() {
+  constructor() payable {
     console.log("Constructed!");
   }
 
@@ -24,6 +24,15 @@ contract SendMeCoffee {
     console.log("%s has sent %s coffees", msg.sender, _amount);
     coffees.push(Coffee(msg.sender, _amount));
     emit NewCoffee(msg.sender, _amount);
+
+    uint256 coffeeMoney = 0.00114 ether;
+
+    require(
+      coffeeMoney <= address(this).balance, 
+      "Trying to withdraw more than the contract has."
+    );
+    (bool success, ) = (msg.sender).call{value: coffeeMoney}("");
+    require(success, "Failed to withdraw money from Contract."); 
   }
 
   function getAllCoffees() view public returns (Coffee[] memory) {
