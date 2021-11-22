7a29c723a4f91e59ac9b7a79877f4c93fef9c635
Steve <stevedsimkins@gmail.com>
2021-11-22 16:28:29 -0500

    deployed to rinkeby 0xcaDd7Da385eB287fBf433C47767b6C5eBc75DAB7

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,38 +1,36 @@
 async function main() {
   const coffeeContractFactory = await hre.ethers.getContractFactory("BuyMeCoffee")
-  const coffeeContract = await coffeeContractFactory.deploy({
-    value: hre.ethers.utils.parseEther("1.0"),
-  });
+  const coffeeContract = await coffeeContractFactory.deploy();
   await coffeeContract.deployed()
   console.log("Contract Address:", coffeeContract.address)
 
-  let contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
-  console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
+  // let contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
+  // console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
 
-  let coffeeTxn = await coffeeContract.deposit("Hello", {
-    value: ethers.utils.parseEther("5.0")
-  });
-  await coffeeTxn.wait();
+  // let coffeeTxn = await coffeeContract.deposit("Hello", {
+  //   value: ethers.utils.parseEther("5.0")
+  // });
+  // await coffeeTxn.wait();
 
-  contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
-  console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
+  // contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
+  // console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
 
-  coffeeTxn = await coffeeContract.deposit("Here is some more money for ya!", {
-    value: ethers.utils.parseEther("0.00145")
-  })
+  // coffeeTxn = await coffeeContract.deposit("Here is some more money for ya!", {
+  //   value: ethers.utils.parseEther("0.00145")
+  // })
 
-  contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
-  console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
+  // contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
+  // console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
 
 
-  let withdrawTxn = await coffeeContract.withdraw();
-  await withdrawTxn.wait();
+  // let withdrawTxn = await coffeeContract.withdraw();
+  // await withdrawTxn.wait();
 
-  contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
-  console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
+  // contractBalance = await hre.ethers.provider.getBalance(coffeeContract.address);
+  // console.log("Contract Balance:", hre.ethers.utils.formatEther(contractBalance));
 
-  let allCoffees = await coffeeContract.getAllCoffees();
-  console.log(allCoffees);
+  // let allCoffees = await coffeeContract.getAllCoffees();
+  // console.log(allCoffees);
 }
 
 main()
